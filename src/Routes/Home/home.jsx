4e77325c72bb126bea1855be999d1b8bde5990ef
dheena <deenatamilnathan007@gmail.com>
2023-05-05 10:12:26 +0530

    working home

---
+++ src/Routes/Home/home.jsx
@@ -0,0 +1,37 @@
+
+
+
+const Home = ()=>{
+        const Data = [{
+            id:1,
+            images:"https://images.pexels.com/photos/1122411/pexels-photo-1122411.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+            id:2,
+            images:"https://images.pexels.com/photos/1910521/pexels-photo-1910521.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+            id:3,
+            images:"https://images.pexels.com/photos/2850286/pexels-photo-2850286.jpeg?auto=compress&cs=tinysrgb&w=600"
+        }
+    ];
+    return (
+      <div>
+        <div
+          className="w-full h-screen bg-no-repeat bg-cover text-white text-8xl flex items-center justify-center "
+          style={{
+            backgroundImage: `url(https://images.pexels.com/photos/154243/pexels-photo-154243.jpeg?auto=compress&cs=tinysrgb&w=600)`,
+          }}
+        >
+          Photographer
+        </div>
+        <div>
+          <img src={Data.images} alt="images" />
+        </div>
+      </div>
+    );
+
+
+};
+
+export default Home;
